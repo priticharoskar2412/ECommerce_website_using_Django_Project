0b0cb60b05cee77b2447d352036b9a71563e71a2
priti <datar.charoskarpriti@gmail.com>
2025-10-30 17:52:52 +0530

    Initial commit - project files

--- Ecommerce_Website/ecommerce/settings.py
+++ Ecommerce_Website/ecommerce/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*8_$d3+ve+g7mi$y(q7j470zsnnse3b$q9)gf_m2vg=cw7c8fz'
+SECRET_KEY = 'django-insecure-8082oilm1c=$4z_r4+47(l#43-(cp0bc9!5rdiv8=712ljg(n5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
