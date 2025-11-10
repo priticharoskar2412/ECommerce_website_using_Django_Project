63f0a513d8fe4d264760764bfae6180a73b6e74c
Rutujaaware <rutujabjadhav1999@gmail.com>
2025-11-10 11:54:44 +0530

    Update models.py

--- Ecommerce_Website/products/models.py
+++ Ecommerce_Website/products/models.py
@@ -62,5 +62,10 @@ class Product(models.Model):
             return round(((self.price - self.discount_price) / self.price) * 100, 2)
         return 0
 
+    class WishlistItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='wishlist_items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='wished_by')
+    added_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):   
         return self.name
