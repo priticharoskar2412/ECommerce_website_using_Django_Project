687ade10b398edadb2d4c5d0fd9079069f47d5ad
Rutujaaware <rutujabjadhav1999@gmail.com>
2025-11-17 14:57:57 +0530

    Update admin.py

--- Ecommerce_Website/products/admin.py
+++ Ecommerce_Website/products/admin.py
@@ -1,23 +1,29 @@
 from django.contrib import admin
-from .models import Category, Product,WishlistItem
-
-# Register your models here.
-
+from .models import Category, Product, WishlistItem
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'is_active', 'created_at')
+    list_display = ('id', 'name', 'is_active', 'created_at')
     prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
+    list_filter = ('is_active',)
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'price', 'stock', 'available', 'created_at')
+    list_display = ('id', 'name', 'category', 'price', 'available', 'brand', 'created_at')
     list_filter = ('available', 'category', 'brand')
-    search_fields = ('name', 'sku', 'tags')
+    search_fields = ('name', 'sku', 'tags', 'brand')
     prepopulated_fields = {'slug': ('name',)}
+    list_editable = ('available',)
+
 
 @admin.register(WishlistItem)
-class WishlistAdmin(admin.ModelAdmin):
-    list_display = ('user','product','added_at')
+class WishlistItemAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product', 'added_at')
+    list_filter = ('user', 'product')
+
+
+
+
 
