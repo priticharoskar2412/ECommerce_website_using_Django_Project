40e51bebb9a45e6b642372755ebd57740d6abcb8
Rutujaaware <rutujabjadhav1999@gmail.com>
2025-11-17 15:00:45 +0530

    Update views.py

--- Ecommerce_Website/products/views.py
+++ Ecommerce_Website/products/views.py
@@ -1,4 +1,4 @@
-rom django.shortcuts import render,redirect,get_object_or_404
+from django.shortcuts import render,redirect,get_object_or_404
 
 # Create your views here.
 from django.urls import reverse_lazy
