9eded6bb555539c3462b7a6ba2ab49ce9d758752
AkashthakurAT7206 <akashthakur7206@gmail.com>
2025-11-18 12:09:44 +0530

    add dropdown in state

--- Ecommerce_Website/orders/forms.py
+++ Ecommerce_Website/orders/forms.py
@@ -2,6 +2,49 @@ from django import forms
 from .models import Order, OrderItem
 
 
+# Define STATE_CHOICES outside the class
+STATE_CHOICES = [
+    ('', 'Select State'),
+    ('AN', 'Andaman and Nicobar Islands'),
+    ('AP', 'Andhra Pradesh'),
+    ('AR', 'Arunachal Pradesh'),
+    ('AS', 'Assam'),
+    ('BR', 'Bihar'),
+    ('CH', 'Chandigarh'),
+    ('CT', 'Chhattisgarh'),
+    ('DN', 'Dadra and Nagar Haveli'),
+    ('DD', 'Daman and Diu'),
+    ('DL', 'Delhi'),
+    ('GA', 'Goa'),
+    ('GJ', 'Gujarat'),
+    ('HR', 'Haryana'),
+    ('HP', 'Himachal Pradesh'),
+    ('JK', 'Jammu and Kashmir'),
+    ('JH', 'Jharkhand'),
+    ('KA', 'Karnataka'),
+    ('KL', 'Kerala'),
+    ('LA', 'Ladakh'),
+    ('LD', 'Lakshadweep'),
+    ('MP', 'Madhya Pradesh'),
+    ('MH', 'Maharashtra'),
+    ('MN', 'Manipur'),
+    ('ML', 'Meghalaya'),
+    ('MZ', 'Mizoram'),
+    ('NL', 'Nagaland'),
+    ('OR', 'Odisha'),
+    ('PY', 'Puducherry'),
+    ('PB', 'Punjab'),
+    ('RJ', 'Rajasthan'),
+    ('SK', 'Sikkim'),
+    ('TN', 'Tamil Nadu'),
+    ('TG', 'Telangana'),
+    ('TR', 'Tripura'),
+    ('UP', 'Uttar Pradesh'),
+    ('UT', 'Uttarakhand'),
+    ('WB', 'West Bengal'),
+]
+
+
 class OrderForm(forms.ModelForm):
     """Form for creating/updating orders"""
     
@@ -26,9 +69,8 @@ class OrderForm(forms.ModelForm):
                 'class': 'form-control',
                 'placeholder': 'City'
             }),
-            'shipping_state': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'State'
+            'shipping_state': forms.Select(choices=STATE_CHOICES, attrs={
+                'class': 'form-select'
             }),
             'shipping_zip_code': forms.TextInput(attrs={
                 'class': 'form-control',
@@ -61,6 +103,7 @@ class OrderForm(forms.ModelForm):
         return zip_code
 
 
+
 class OrderItemForm(forms.ModelForm):
     """Form for adding items to an order"""
     
@@ -76,4 +119,3 @@ class OrderItemForm(forms.ModelForm):
             }),
             'price': forms.HiddenInput(),
         }
-
